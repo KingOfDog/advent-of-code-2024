d16c230b61187bc5502f3e7925ae1be300807781
KingOfDog <logins@kingofdog.de>
2024-12-02 12:49:52 +0000

    Day 2 complete

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,48 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n")
+    |> Enum.count(fn line ->
+      numbers = String.split(line) |> Enum.map(fn x -> Integer.parse(x) |> elem(0) end)
+
+      diffs =
+        Enum.chunk_every(numbers, 2, 1, :discard)
+        |> Enum.map(fn [a, b] -> b - a end)
+
+      increasing_or_decreasing =
+        Enum.all?(diffs, fn x -> x < 0 end) or Enum.all?(diffs, fn x -> x > 0 end)
+
+      at_most_three = Enum.all?(diffs, fn x -> abs(x) <= 3 end)
+
+      not Enum.empty?(numbers) and increasing_or_decreasing and at_most_three
+    end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.count(fn line ->
+      numbers = String.split(line) |> Enum.map(fn x -> Integer.parse(x) |> elem(0) end)
+
+      options =
+        ([numbers] ++
+           (numbers
+            |> Stream.with_index()
+            |> Enum.map(fn {_x, i} -> List.delete_at(numbers, i) end)))
+        |> IO.inspect(charlists: :as_lists)
+
+      Enum.any?(options, fn list ->
+        diffs =
+          Enum.chunk_every(list, 2, 1, :discard)
+          |> Enum.map(fn [a, b] -> b - a end)
+
+        increasing_or_decreasing =
+          Enum.all?(diffs, fn x -> x < 0 end) or Enum.all?(diffs, fn x -> x > 0 end)
+
+        at_most_three = Enum.all?(diffs, fn x -> abs(x) <= 3 end)
+
+        not Enum.empty?(list) and increasing_or_decreasing and at_most_three
+      end)
+    end)
   end
 end
