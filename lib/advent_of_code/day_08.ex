f77c1fea44387c4a99536d7088b0ec1809c7337c
KingOfDog <logins@kingofdog.de>
2024-12-10 12:49:31 +0100

    Day 8 complete

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,75 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  def part1(args) do
+    {coords, frequencies} = parse(args)
+
+    frequencies
+    |> Enum.flat_map(fn {_, antennas} ->
+      antennas
+      |> Enum.flat_map(fn {ax, ay} ->
+        antennas
+        |> List.delete({ax, ay})
+        |> Enum.map(fn {bx, by} ->
+          {dx, dy} = {bx - ax, by - ay}
+          {ax - dx, ay - dy}
+        end)
+      end)
+    end)
+    |> Enum.filter(&Map.has_key?(coords, &1))
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
-  def part2(_args) do
+  defp parse(args) do
+    coords =
+      args
+      |> parse_grid()
+      |> grid_to_coords()
+
+    frequencies =
+      coords
+      |> Enum.group_by(&elem(&1, 1), &elem(&1, 0))
+      |> Map.delete(?.)
+
+    coords = coords |> Map.new()
+
+    {coords, frequencies}
+  end
+
+  defp parse_grid(string) do
+    string
+    |> String.split()
+    |> Enum.map(&String.to_charlist/1)
+  end
+
+  defp grid_to_coords(grid) do
+    grid
+    |> Enum.with_index(fn row, y ->
+      row
+      |> Enum.with_index(fn c, x ->
+        {{x, y}, c}
+      end)
+    end)
+    |> Enum.concat()
+  end
+
+  def part2(args) do
+    {coords, frequencies} = parse(args)
+
+    frequencies
+    |> Stream.flat_map(fn {_, antennas} ->
+      antennas
+      |> Stream.flat_map(fn {ax, ay} ->
+        antennas
+        |> List.delete({ax, ay})
+        |> Stream.flat_map(fn {bx, by} ->
+          {dx, dy} = {bx - ax, by - ay}
+
+          Stream.iterate({ax, ay}, fn {x, y} -> {x - dx, y - dy} end)
+          |> Stream.take_while(&Map.has_key?(coords, &1))
+        end)
+      end)
+    end)
+    |> Stream.uniq()
+    |> Enum.count()
   end
 end
